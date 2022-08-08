4bad0b78838e3e6cae4a3164af0e0b99b49e2166
AshladBP <imadbenbp@gmail.com>
2022-08-08 21:27:26 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+class MulpyVersus:
+    """Synchronous Multiversus API wrapper.
+    Usage Example:
+        ::
+            from jikanpy import Jikan
+            jikan = Jikan()
+    Attributes:
+        base: The base URL of the Jikan API being accessed.
+        session: The Requests session.
+    """
+    def __init__(self):
+        pass
