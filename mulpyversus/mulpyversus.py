e44bbb4bef0c1cee7e67ef0c2ebc8e4effddf373
AshladBP <imadbenbp@gmail.com>
2022-08-08 21:35:28 +0200

    edit

---
+++ mulpyversus/mulpyversus.py
@@ -0,0 +1,10 @@
+class MulpyVersus:
+    """Synchronous Multiversus API wrapper.
+    Usage Example:
+        ::
+            from mulpyversus import MulpyVersus
+            mulpyversus = MulpyVersus()
+    Attributes:
+    """
+    def __init__(self):
+        pass
